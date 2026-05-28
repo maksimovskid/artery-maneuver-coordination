d21cf5fa12e81bef89462ed42cdba5a50d1de6cb
Daniel <daniel.maksimovski@carissma.eu>
2026-05-28 18:04:15 +0200

    Add McApplication execution state skeleton

--- src/artery/application/mcm/McApplication.cc
+++ src/artery/application/mcm/McApplication.cc
@@ -5,5 +5,35 @@ namespace artery
 namespace mcm
 {
 
+void McApplication::initialize(traci::VehicleController* controller)
+{
+    mVehicleController = controller;
+}
+
+void McApplication::tick(omnetpp::SimTime)
+{
+    // TODO: later apply execution commands from parsed MCM Execute/Cancel events.
+}
+
+void McApplication::clearCommand()
+{
+    mPendingCommand = CommandKind::None;
+    mExecutionState = ExecutionState::Idle;
+    mTargetSpeed = 0.0;
+    mCommandDuration = 0.0;
+    mRestoreSpeed = 0.0;
+    mRestoreMaxSpeed = 0.0;
+}
+
+bool McApplication::hasActiveExecution() const
+{
+    return mExecutionState == ExecutionState::Pending || mExecutionState == ExecutionState::Executing;
+}
+
+void McApplication::applyCommand()
+{
+    // TODO: later use VehicleController hooks for DecelerateTo and RestoreNormalSpeed commands.
+}
+
 }  // namespace mcm
 }  // namespace artery
--- src/artery/application/mcm/McApplication.h
+++ src/artery/application/mcm/McApplication.h
@@ -1,6 +1,13 @@
 #ifndef ARTERY_MCM_MCAPPLICATION_H_
 #define ARTERY_MCM_MCAPPLICATION_H_
 
+#include <omnetpp/simtime.h>
+
+namespace traci
+{
+class VehicleController;
+}
+
 namespace artery
 {
 namespace mcm
@@ -11,9 +18,47 @@ class McApplication
 public:
     McApplication() = default;
 
+    void initialize(traci::VehicleController*);
+    void tick(omnetpp::SimTime now);
+    void clearCommand();
+    bool hasActiveExecution() const;
+
     // TODO: add negotiation strategy and maneuver state handling.
     // TODO: add scenario-specific maneuver coordination behavior.
     // TODO: integrate TrajectoryPlanner helpers after the service-level MCM plumbing is stable.
+
+private:
+    enum class Role {
+        None,
+        RequestingVehicle,
+        CooperatingVehicle
+    };
+
+    enum class ExecutionState {
+        Idle,
+        Pending,
+        Executing,
+        Complete,
+        Cancelled
+    };
+
+    enum class CommandKind {
+        None,
+        DecelerateTo,
+        RestoreNormalSpeed
+    };
+
+    void applyCommand();
+
+    traci::VehicleController* mVehicleController = nullptr;
+    Role mRole = Role::None;
+    ExecutionState mExecutionState = ExecutionState::Idle;
+    CommandKind mPendingCommand = CommandKind::None;
+
+    double mTargetSpeed = 0.0;
+    double mCommandDuration = 0.0;
+    double mRestoreSpeed = 0.0;
+    double mRestoreMaxSpeed = 0.0;
 };
 
 }  // namespace mcm
