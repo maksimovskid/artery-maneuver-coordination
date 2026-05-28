adc4f258ce6c15219a88f6a7016d430bb514f263
Daniel <daniel.maksimovski@carissma.eu>
2026-05-28 17:53:51 +0200

    Add VehicleController hooks for MCM integration

--- src/artery/traci/VehicleController.cc
+++ src/artery/traci/VehicleController.cc
@@ -57,10 +57,92 @@ void VehicleController::changeTarget(const std::string& edge)
     m_traci->vehicle.changeTarget(getId(), edge);
 }
 
+void VehicleController::changeLane(const std::string&, int laneIndex, double duration) const
+{
+    m_traci->vehicle.changeLane(getId(), laneIndex, duration);
+}
+
+void VehicleController::changeSublane(const std::string&, double latDist) const
+{
+    m_traci->vehicle.changeSublane(getId(), latDist);
+}
+
+void VehicleController::moveTo(const std::string&, const std::string& laneID, double position, int reason) const
+{
+    m_traci->vehicle.moveTo(getId(), laneID, position, reason);
+}
+
+void VehicleController::moveToXY(const std::string&, const std::string& edgeID, int lane, double x, double y, double angle, int keepRoute) const
+{
+    m_traci->vehicle.moveToXY(getId(), edgeID, lane, x, y, angle, keepRoute);
+}
+
+void VehicleController::slowDown(const std::string&, double speed, double duration) const
+{
+    m_traci->vehicle.slowDown(getId(), speed, duration);
+}
+
+void VehicleController::setLaneChangeMode(const std::string&, int mode) const
+{
+    m_traci->vehicle.setLaneChangeMode(getId(), mode);
+}
+
+void VehicleController::setSpeedMode(const std::string&, int mode) const
+{
+    m_traci->vehicle.setSpeedMode(getId(), mode);
+}
+
 artery::Position VehicleController::getPositionSumo() const
 {
     return traci::position_cast_sumo(m_cache->get<libsumo::VAR_POSITION>());
 }
 
+std::string VehicleController::getRouteID() const
+{
+    return m_traci->vehicle.getRouteID(getId());
+}
+
+int VehicleController::getRouteIndex() const
+{
+    return m_traci->vehicle.getRouteIndex(getId());
+}
+
+std::vector<std::string> VehicleController::getRoute() const
+{
+    return m_traci->vehicle.getRoute(getId());
+}
+
+std::vector<std::string> VehicleController::getEdges(const std::string& routeID) const
+{
+    return m_traci->route.getEdges(routeID);
+}
+
+double VehicleController::getLength(const std::string& laneID) const
+{
+    return m_traci->lane.getLength(laneID);
+}
+
+void VehicleController::setAccel(const std::string&, double accel) const
+{
+    // Old MCM call sites sometimes pass a vehicle ID here; mutate this vehicle's current type for compatibility.
+    m_traci->vehicletype.setAccel(getTypeId(), accel);
+}
+
+void VehicleController::setDecel(const std::string&, double decel) const
+{
+    // Old MCM call sites sometimes pass a vehicle ID here; mutate this vehicle's current type for compatibility.
+    m_traci->vehicletype.setDecel(getTypeId(), decel);
+}
+
+void VehicleController::setTau(const std::string&, double tau) const
+{
+    // Old MCM call sites sometimes pass a vehicle ID here; mutate this vehicle's current type for compatibility.
+    m_traci->vehicletype.setTau(getTypeId(), tau);
+}
+
+void VehicleController::setType(const std::string&, const std::string& typeID) const
+{
+    m_traci->vehicle.setType(getId(), typeID);
+}
 
 } // namespace traci
--- src/artery/traci/VehicleController.h
+++ src/artery/traci/VehicleController.h
@@ -4,6 +4,8 @@
 #include "artery/traci/Controller.h"
 #include "artery/traci/VehicleType.h"
 
+#include <vector>
+
 namespace traci
 {
 
@@ -19,6 +21,8 @@ public:
     const VehicleType& getVehicleType() const;
     const std::string getVehicleClass() const;
 
+    using Controller::getLength;
+
     Velocity getMaxSpeed() const;
     void setMaxSpeed(Velocity);
     void setSpeed(Velocity);
@@ -26,7 +30,26 @@ public:
 
     void changeTarget(const std::string& edge);
 
+    void changeLane(const std::string& vehicleID, int laneIndex, double duration) const;
+    void changeSublane(const std::string& vehicleID, double latDist) const;
+    void moveTo(const std::string& vehicleID, const std::string& laneID, double position, int reason = libsumo::MOVE_TELEPORT) const;
+    void moveToXY(const std::string& vehicleID, const std::string& edgeID, int lane, double x, double y, double angle, int keepRoute) const;
+    void slowDown(const std::string& vehicleID, double speed, double duration) const;
+    void setLaneChangeMode(const std::string& vehicleID, int mode) const;
+    void setSpeedMode(const std::string& vehicleID, int mode) const;
+
     artery::Position getPositionSumo() const;
+
+    std::string getRouteID() const;
+    int getRouteIndex() const;
+    std::vector<std::string> getRoute() const;
+    std::vector<std::string> getEdges(const std::string& routeID) const;
+    double getLength(const std::string& laneID) const;
+
+    void setAccel(const std::string& typeID, double accel) const;
+    void setDecel(const std::string& typeID, double decel) const;
+    void setTau(const std::string& typeID, double tau) const;
+    void setType(const std::string& vehicleID, const std::string& typeID) const;
 
 private:
     VehicleType m_type;
@@ -35,4 +58,3 @@ private:
 } // namespace traci
 
 #endif /* VEHICLECONTROLLER_H_AXBS5NQM */
-
